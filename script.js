90ed8b0ef1af85fc1ffcc548a8cc2de62790145a
Danilo dos Reis Amaral <danreis.ctt@gmail.com>
2023-06-20 11:41:42 -0300

    criar primeira versão do sistema de pausa

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
 const form = document.querySelector('#main-form');
+const breakIndicator = document.querySelector(".break")
 const result = document.querySelector('#result');
 const resetBtn = document.querySelector("#reset-btn");
 const formatTime = (num) => num.toString().length < 2 ? '0' + num : num;
 let timer = null;
 let timeNow = 0;
+const options = {
+  25: {seconds: 1, pause: 300},
+  50: {seconds: 3000, pause: 600}
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-
   const data = new FormData(form);
-  let minutes = null;
+  let option = null;
  
   for(let entries of data) {
-    minutes = Number(entries[1]);
+    option = options[entries[1]];
   }
 
-  if(!minutes) return;
+  if(!option) return;
   reset();
-  timer = setInterval(()=> updateTime(minutes), 1000);
+  timer = setInterval(()=> updateTime(option), 1000);
 });
 
 resetBtn.addEventListener('click', reset)
 
-function updateTime(seconds) {
+function updateTime({seconds, pause}) {
   let missingTime = seconds - timeNow;
 
-  if(missingTime === 0) return reset();
+  if(missingTime === 0) {
+    breakIndicator.classList.add("active");
+    reset();
+    timer = setInterval(()=> setBreak(pause), 1000);
+  }
 
   updateDisplay(missingTime);
   timeNow += 1;
 }
 
 function updateDisplay(timeInSeconds) {
+  console.log(timeInSeconds)
   const minutes = Math.floor(timeInSeconds / 60);
   const seconds = timeInSeconds % 60;
 
@@ -42,6 +51,12 @@ function reset() {
   clearInterval(timer);
   updateDisplay(0);
   timeNow = 0;
+}
+
+function setBreak(seconds){
+  let missingTime = seconds - timeNow;
+  updateDisplay(missingTime);
+  timeNow += 1;
 }
 
 // animações dos botões
