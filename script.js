55c97f2846002f3e839e209cb9dbe2960b88fc47
Danilo dos Reis Amaral <danreis.ctt@gmail.com>
2023-11-13 19:16:35 -0300

    adicionar driver.js e fazer pequena mudança
    O módulo que fo driver.js permite adicionar de forma super simples um guia para o usuário, para facilitar o entendimento de algo. Nesse caso, permite que o usuário conheça o que cada botão do pomodoro faz.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const form = document.querySelector('#main-form');
 const breakIndicator = document.querySelector(".break")
 const result = document.querySelector('#result');
@@ -9,6 +10,26 @@ const options = {
   25: {seconds: 1500, pause: 300},
   50: {seconds: 3000, pause: 600}
 }
+// usign driver to guide the user
+const driver = window.driver.js.driver;
+
+console.log(document.querySelector('#main-form input[type="radio"]').value)
+console.log(document.querySelector('#main-form input'))
+
+const driverObj = driver({
+  showProgress: true,
+  steps: [
+    { element: '#box-25-minutes', popover: { title: '25 Minutes Selector', description: 'Initializes a timer with 25 minutes of focus and 5 minutes of rest.' } },
+    { element: '#box-50-minutes', popover: { title: '50 Minutes Selector', description: 'Initializes a timer with 50 minutes of focus and 10 minutes of rest.' } },
+    { element: '.break', popover: { title: 'Indicator "Break"', description: 'If it is lit, it indicates that it is time to take a break.'}},
+    { element: '.display-time', popover: { title: 'Tempo restante', description: 'Indicates remaining focus time or pause time.' } },
+    { element: '#reset-btn', popover: { title: 'Reset Button', description: 'Reset the timer.' } },
+    { element: '#play-btn', popover: { title: 'Play Button', description: 'Start counting.' } },
+    { popover: { title: 'Notice', description: 'As the objective of the pomodoro is to do the sequence without pausing, there is no pause button.' } },
+  ]
+});
+
+driverObj.drive();
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -19,7 +40,7 @@ form.addEventListener('submit', (e) => {
     option = options[entries[1]];
   }
 
-  if(!option) return;
+  if(!option || !option && timer) return;
   reset();
   timer = setInterval(()=> updateTime(option), 1000);
 });
@@ -40,7 +61,6 @@ function updateTime({seconds, pause}) {
 }
 
 function updateDisplay(timeInSeconds) {
-  console.log(timeInSeconds)
   const minutes = Math.floor(timeInSeconds / 60);
   const seconds = timeInSeconds % 60;
 
